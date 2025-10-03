0c5e668ba87d1347d321b0cfb969299a6e1baa5e
ruohl <rudolfgarciabohl@gmail.com>
2025-10-03 20:48:38 -0300

    fix: algunos arreglos y separaciones

--- scripts/jugador/player.gd
+++ scripts/jugador/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const MAX_SPEED : float = 200
 const JUMP_VELOCITY = -400.0
+var ultimaDireccion: bool
 
 @export_range(0.0, MAX_SPEED, 1.0)
 var speed : float = 160.0
@@ -11,9 +12,9 @@ var jump_velocity : float = -300.0
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	manejarAnimacion()
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		sprite.play("Jump")
 
 	if Input.is_action_pressed("saltar") and is_on_floor():
 		velocity.y = jump_velocity
@@ -24,3 +25,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	move_and_slide()
+
+func manejarAnimacion() -> void:
+	#Manejo de dirección
+	if (velocity != Vector2.ZERO) and is_on_floor():
+		ultimaDireccion = velocity.x < 0
+
+	if (ultimaDireccion):
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+
+	if not is_on_floor():
+		sprite.play("Jump")
+	if !velocity:
+		sprite.play("Idle")
+	else:
+		sprite.play("Run")
